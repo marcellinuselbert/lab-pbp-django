df32fcdb36bc388b40631a60db355f6832e9b1aa
marcellinuselbert <elbertmarcellinus@gmail.com>
2022-09-15 13:40:21 +0700

    Add show xml by id method

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from wishlist.views import show_wishlist, show_xml, show_json, show_json_by_id
+from wishlist.views import show_wishlist, show_xml, show_json, show_json_by_id, show_xml_by_id
 
 app_name = 'wishlist'
 
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', show_wishlist, name='show_wishlist'),
     path('xml/',show_xml,name="show_xml"),
     path('json/',show_json,name="show_json"),
-    path('json/<int:id>', show_json_by_id, name="show_json_by_id")
-    
+    path('json/<int:id>', show_json_by_id, name="show_json_by_id"),
+    path('xml/<int:id>', show_xml_by_id, name="show_xml_by_id")
+
 ]
