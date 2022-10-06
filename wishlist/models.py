9b966bddbf73ce7d91c84a4e2a3f14cf9f5e025c
marcellinuselbert <elbertmarcellinus@gmail.com>
2022-10-06 11:16:33 +0700

    Implement lab5

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-
+# from django.contrib.auth.models import User
 class BarangWishlist(models.Model):
+    # user = models.ForeignKey(User,on_delete=models.CASCADE)
     nama_barang = models.CharField(max_length=50)
     harga_barang = models.IntegerField()
     deskripsi = models.TextField()
--- wishlist/views.py
+++ wishlist/views.py
@@ -8,6 +8,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login,logout
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
 
 @login_required(login_url='/wishlist/login/')
 def show_wishlist(request):
@@ -18,6 +19,16 @@ def show_wishlist(request):
     'last_login': request.COOKIES['last_login'],
     }
     return render(request, "wishlist.html",context)
+
+@login_required(login_url='/wishlist/login/')
+def show_wishlist_ajax(request):
+    data_barang_wishlist = BarangWishlist.objects.all()
+    context = {
+    'list_barang': data_barang_wishlist,
+    'nama': 'Marcellinus Elbert',
+    'last_login': request.COOKIES['last_login'],
+    }
+    return render(request, "wishlist_ajax.html",context)
 
 def show_xml(request):
     data = BarangWishlist.objects.all()
@@ -69,3 +80,21 @@ def logout_user(request):
     response = HttpResponseRedirect(reverse('wishlist:login'))
     response.delete_cookie('last_login')
     return response
+
+@login_required(login_url='login/')
+@csrf_exempt
+def show_create_wishlist(request):
+    if request.method == 'POST':
+        nama_barang = request.POST.get('nama_barang')
+        harga_barang = request.POST.get('harga_barang')
+        deskripsi = request.POST.get('deskripsi')
+        wishlist = BarangWishlist.objects.create(nama_barang=nama_barang,deskripsi=deskripsi,harga_barang=harga_barang)
+        response = HttpResponseRedirect(reverse("wishlist:show_wishlist_ajax")) 
+
+        # data_barang_wishlist = BarangWishlist.objects.all()
+        # print(data_barang_wishlist)
+
+
+        return response
+
+    # return render(request, "create.html")
